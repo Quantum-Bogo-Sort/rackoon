10f8b6962e6ebdaef649462f130d3b1bf35f75b3
Faux <nikst0r1963@gmail.com>
2021-02-27 18:06:06 +0200

    added category buttons

--- public/web.js
+++ public/web.js
@@ -8,12 +8,10 @@ function addCartButtonListeners() {
     const cartModal = document.querySelector('#cart');
     const closeCart = document.querySelector('#close-cart');
     const cartButton = document.querySelector('#cart-button');
-    console.log(cartButton);
     cartButton.addEventListener('click', () => {
         cartModal.style.display = "block";
     });
 
-    console.log('aa');
     closeCart.addEventListener('click', () => {
         cartModal.style.display = "none";
     });
@@ -22,6 +20,17 @@ function addCartButtonListeners() {
         if (e.target == cartModal) {
             cartModal.style.display = "none";
         }
+    });
+
+    const ingredientsBtn = document.querySelector('#ingredients-btn');
+    const recipesBtn = document.querySelector('#recipes-btn');
+
+    ingredientsBtn.addEventListener('click', () => {
+        setActiveCategory('ingredients');
+    });
+
+    recipesBtn.addEventListener('click', () => {
+        setActiveCategory('recipes');
     });
 }
 
@@ -47,6 +56,14 @@ async function onLoggedIn(user) {
     appElement.style.display = "block";
 
     app.mount('#app');
+
+    setActiveCategory('ingredients')
+    addCartButtonListeners();
+}
+
+async function addFoods() {
+    clearShopItems();
+
     let foods = await getFoods();
 
     let i = 0;
@@ -56,15 +73,107 @@ async function onLoggedIn(user) {
         addFoodElement(foodData, element.id);
         i++;
     });
+}
 
-    addCartButtonListeners();
+function setActiveCategory(string) {
+    const ingredientsBtn = document.querySelector('#ingredients-btn');
+    const recipesBtn = document.querySelector('#recipes-btn');
+
+    if (string === 'ingredients') {
+        if (ingredientsBtn.classList.contains('active-btn')) return false;
+        else {
+            recipesBtn.classList.remove('active-btn');
+            ingredientsBtn.classList.add('active-btn');
+
+            addFoods();
+            return true;
+        }
+    } else if (string === 'recipes') {
+        if (recipesBtn.classList.contains('active-btn')) return false;
+        else {
+            ingredientsBtn.classList.remove('active-btn');
+            recipesBtn.classList.add('active-btn');
+
+            addRecipes();
+            return true;
+        }
+    }
+    return false;
+}
+
+function clearShopItems() {
+    const shopItems = document.querySelector('.shop-items');
+
+    shopItems.innerHTML = '';
+}
+
+async function addRecipes() {
+    const shopItems = document.querySelector('.shop-items');
+    clearShopItems();
+
+    const recipes = await filterRecipes();
+
+    let i = 0;
+    for (recipe of recipes) {
+        if (i % 4 === 0) addRow();
+        addRecipeElement(recipe);
+        i++;
+    }
+}
+
+function addRecipeElement(recipe) {
+    console.log(recipe.recipe.data().name);
+
+    const lastRow = document.querySelector('.card-container:last-child');
+
+    const foodItemDiv = document.createElement('div');
+    foodItemDiv.classList.add('food-item');
+
+    const foodName = document.createElement('h3');
+    foodName.textContent = recipe.recipe.data().name;
+
+    // const foodPrice = document.createElement('p');
+
+    // const date = new Date(foodData.expiration.seconds * 1000);
+    // const daysUntilExpirationDiv = document.createElement('h4');
+    // daysUntilExpirationDiv.textContent = `Best before: ${getFormattedDate(date)}`;
+
+    // if (foodData.reduced) {
+    //     foodPrice.classList.add('discount-price');
+    //     foodPrice.textContent = `$${Number.parseFloat(foodData.reduced).toFixed(2)} `;
+
+    //     const oldPrice = document.createElement('span');
+    //     oldPrice.textContent = `$${Number.parseFloat(foodData.price).toFixed(2)}`;
+    //     foodPrice.appendChild(oldPrice);
+
+    // } else {
+    //     foodPrice.textContent = `$${Number.parseFloat(foodData.price).toFixed(2)}`;
+    // }
+
+    // foodItemDiv.append(foodName, foodPrice, daysUntilExpirationDiv);
+    const offers = document.createElement('p');
+    offers.textContent = `Ingredient offers: ${recipe.offers.length}`;
+
+    foodItemDiv.append(foodName, offers);
+    const button = document.createElement('button');
+    button.textContent = "Add to cart";
+    button.classList.add('btn');
+    button.addEventListener('click', () => {
+        recipe.offers.forEach(id => {
+            addToCart(id);
+        });
+    });
+
+    foodItemDiv.appendChild(button);
+
+    lastRow.appendChild(foodItemDiv);
 }
 
 function getNumOfDays(dateFuture, datePast)
 {
-  let diff = Number(dateFuture) - Number(datePast);
-  //Seconds to days
-  return Math.floor((((diff)/60)/60)/24);
+    let diff = Number(dateFuture) - Number(datePast);
+    //Seconds to days
+    return Math.floor((((diff)/60)/60)/24);
 }
 
 function addRow() {
@@ -72,6 +181,17 @@ function addRow() {
     row.classList.add('card-container');
 
     document.querySelector('.shop-items').appendChild(row);
+}
+
+function getFormattedDate(date) {
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[date.getMonth()];
+    const day = String(date.getDate()).padStart(2, '0');
+    const year = date.getFullYear();
+    const output = month  + '\n'+ day  + ' ' + year;
+    return output;
 }
 
 function addFoodElement(foodData, id) {
@@ -85,35 +205,23 @@ function addFoodElement(foodData, id) {
 
     const foodPrice = document.createElement('p');
 
-    const days = getNumOfDays(foodData.expiration.seconds, Date.now()/1000);
-    const daysUntilExpirationDiv = document.createElement('h4');
     const date = new Date(foodData.expiration.seconds * 1000);
-
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-    const month = monthNames[date.getMonth()];
-    const day = String(date.getDate()).padStart(2, '0');
-    const year = date.getFullYear();
-    const output = month  + '\n'+ day  + ' ' + year;
-
-    daysUntilExpirationDiv.textContent = `Best before: ${output}`;
-
-    console.log(getNumOfDays(foodData.expiration.seconds, Date.now()/1000));
+    const daysUntilExpirationDiv = document.createElement('h4');
+    daysUntilExpirationDiv.textContent = `Best before: ${getFormattedDate(date)}`;
 
     if (foodData.reduced) {
         foodPrice.classList.add('discount-price');
         foodPrice.textContent = `$${Number.parseFloat(foodData.reduced).toFixed(2)} `;
 
         const oldPrice = document.createElement('span');
-        oldPrice.textContent = `$${foodData.price}`;
+        oldPrice.textContent = `$${Number.parseFloat(foodData.price).toFixed(2)}`;
         foodPrice.appendChild(oldPrice);
 
     } else {
-        foodPrice.textContent = `$${foodData.price}`;
+        foodPrice.textContent = `$${Number.parseFloat(foodData.price).toFixed(2)}`;
     }
 
     foodItemDiv.append(foodName, foodPrice, daysUntilExpirationDiv);
-    console.log(foodData);
     const button = document.createElement('button');
     button.textContent = "Add to cart";
     button.classList.add('btn');
