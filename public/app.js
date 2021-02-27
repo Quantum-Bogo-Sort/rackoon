aa2c741719a79f1191d014921f6f811daae44a73
Faux <nikst0r1963@gmail.com>
2021-02-27 03:10:55 +0200

    fix: add to cart works

--- public/app.js
+++ public/app.js
@@ -54,36 +54,27 @@ function removeFoodByDocID(id)
 async function addToCart(id)
 {
     const db = firebase.firestore();
-    //TODO: error handlionsdefgh
-    const myPromise = new Promise((resolve, reject) => {
-        let toResolve = true;
-        cart.forEach( async (element) =>
-        {
+    const myPromise = new Promise(async (resolve, reject) => {
+        for await (element of cart) {
             let curr = await db.collection("foods").doc(element).get();
             let toCmp = await db.collection("foods").doc(id).get();
-            console.log(curr.data().store + " " + toCmp.data().store);
-            // if(curr.data().store.localeCompare(toCmp.data().store))
+
             if(curr.data().store != toCmp.data().store)
             {
-                console.log("REEEEEEEEEEEEEEEEEe");
-                toResolve = false;
                 resolve(false);
             }
-        });
+        }
         resolve(true);
     });
-    myPromise.then(canAdd => {
-        console.log(canAdd);
 
-        if(canAdd)
-        {
-            cart.push(id);
-        }
-        else
-        {
-            throw 'Element not from the same store';
-        }
-    });
+    const canAdd = await myPromise;
 
-    
+    if(canAdd)
+    {
+        cart.push(id);
+    }
+    else
+    {
+        throw 'Element not from the same store';
+    }
 }
