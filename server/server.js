e0c33d5c82081c04cd43f00543e2001abc053e72
petergerasimov <petergerasimov2000@gmail.com>
2021-02-27 16:17:03 +0200

    automatic price reduction

--- server/server.js
+++ server/server.js
@@ -21,16 +21,61 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-setInterval(printFoode, 500, 'test');
+setInterval(reducePricesInDB, 500, 'test');
 
 //
 async function printFoode() //This is just for testing at the moment
 {
-    const result = await admin
+  const result = await admin
+  .firestore()
+  .collection('foods').get();
+
+  result.forEach(element => {
+      console.log(element.data().name);
+  });
+}
+
+//Returns the number of days between two dates
+function getNumOfDays(dateFuture, datePast)
+{
+  let diff = Number(dateFuture) - Number(datePast);
+  //Seconds to days
+  return Math.floor((((diff)/60)/60)/24);
+}
+function mapRange(input, inMin, inMax, outMin, outMax) {
+  return (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+async function removeFoodByDocID(id)
+{
+  const db = await admin
+    .firestore()
+    .collection('foods').doc(id).delete().then(() => {
+      console.log("Document successfully deleted!");
+  }).catch((error) => {
+      console.error("Error removing document: ", error);
+  });
+}
+
+async function reducePricesInDB()
+{
+  const result = await admin
     .firestore()
     .collection('foods').get();
 
     result.forEach(element => {
-        console.log(element.data().name);
+        diff = getNumOfDays(element.data().expiration._seconds, Date.now()/1000);
+        if(diff < 0)
+        {
+          removeFoodByDocID(element.id);
+        }
+        if(diff <= 7)
+        {
+          let discount = mapRange(diff, 0, 7, 0.75, 0.25);
+          console.log(discount);
+          toReplace = element.data();
+          toReplace.reduced = discount*element.data().price;
+          admin.firestore().collection("foods").doc(element.id).set(toReplace);
+        }
     });
 }
